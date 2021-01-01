8de93d640dead6958dfc3ded5d87f599e32824aa
shatil-rahman <shatil.rahman@mail.mcgill.ca>
2021-01-01 14:02:24 -0500

    Modified drawing code to draw 3D environment

--- Utils/drawObstacles.m
+++ Utils/drawObstacles.m
@@ -9,13 +9,21 @@ function drawObstacles(h,map)
 figure(h)
 hold on;
 
-
-for i = 1:size(map.obstacles,2)
-    R = map.obstacle_sizes(i);
-    obs = map.obstacles(:,i);
-    lp = obs - [R;R];
-    pos = [lp' R R];
-    rectangle('Position',pos','FaceColor','m');
-end
+if size(map.bounds,1) == 2
+    for i = 1:size(map.obstacles,2)
+        R = map.obstacle_sizes(i);
+        obs = map.obstacles(:,i);
+        lp = obs - [R;R];
+        pos = [lp' R R];
+        rectangle('Position',pos','FaceColor','m');
+    end
+else
+    for i = 1:size(map.obstacles,2)
+        R = map.obstacle_sizes(i);
+        obs = map.obstacles(:,i);
+        lp = obs - [R;R];
+        pos = [lp',0];
+        plotcube([R,R,R],pos,1,[1,0,1]);
+    end
 
 end
